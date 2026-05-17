5d76b3b0f76f8e11243c7bbea014b171657aab77
G41240178-NURFITRIA <g41240178@student.polije.ac.id>
2026-05-17 11:53:14 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -389,7 +389,17 @@ body{
     <div class="main">
 
         {{-- HEADER --}}
-        @include('layouts.navbar', ['title' => 'Selamat datang, Admin'])
+        <div class="topbar">
+            <div>
+                <h1>Selamat datang, Admin</h1>
+                <p>Berikut ringkasan aktivitas Anda hari ini</p>
+            </div>
+
+            <div class="top-info">
+                <span>📅 Rabu, 06 Mei 2026</span>
+                <span>⏱️ 14:20 WIB</span>
+            </div>
+        </div>
 
         {{-- STATS --}}
         <div class="stats-row">
